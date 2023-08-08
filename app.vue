e5e8efcfe2e744f34458ded3163332b0f5482d5d
therealtuyen <tuyendev@gmail.com>
2023-08-08 09:49:54 +0700

    initial source

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto mt-10">
+      <!-- Open the modal using ID.showModal() method -->
+      <div class="flex justify-center">
+          <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
+          <dialog id="my_modal_1" class="modal">
+              <form method="dialog" class="modal-box">
+                  <h3 class="font-bold text-lg">Hello!</h3>
+                  <p class="py-4">Press ESC key or click the button below to close</p>
+                  <div class="modal-action">
+                      <!-- if there is a button in form, it will close the modal -->
+                      <button class="btn">Close</button>
+                  </div>
+              </form>
+          </dialog>
+      </div>
+  </div>
+</template>
